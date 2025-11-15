3257a902e38738c4e6e022e1d24063dcd6dcaa5d
ladonijaraze <ladonijaraze@gmail.com>
2025-11-15 19:24:59 +0400

    commit

---
+++ level39/classwork/work.py
@@ -0,0 +1,12 @@
+#1)https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/python
+def repeat_str(repeat, string):
+    return string*repeat
+#2)https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/python
+def remove_char(s):
+    return s[1:-1]
+#3)https://www.codewars.com/kata/515e271a311df0350d00000f/train/python
+def square_sum(numbers):
+    sums = 0
+    for x in numbers:
+        sums += x**2
+    return sums
